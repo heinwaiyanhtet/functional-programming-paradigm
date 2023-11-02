d6415adb5ca06fcea06cb37d8be2299e2d8e8483
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2023-11-02 12:41:40 +0630

    Constructors explicityly returing object values

--- function_method_invoke.js
+++ function_method_invoke.js
@@ -1,3 +1,45 @@
+// Constructors explicitly returning object values 
+
+
+var puppet = 
+{
+    rules:false,
+}
+
+function Emperor()
+{
+    this.rules = true;
+    return puppet;
+}
+
+var emperor = new Emperor();
+
+console.assert(emperor === puppet,"The emperor is merely a puppet");
+console.assert(emperor.rules === false,"The puppet does not know how to rule!");
+
+
+
+
+
+// constructor returing primitive values
+
+function Ninja()
+{
+    this.stulk = function ()
+    {
+        return true;
+    }
+    return 1;
+}
+
+console.assert(Ninja() === 1,"Return value honored when not called as a constructor")
+
+var ninja = new Ninja();
+
+console.assert(typeof ninja === "object","Object returned when called as a constructor");
+console.assert(typeof ninja.stulk === "function","ninja object has a skulk method");
+
+
 // using a constructor to set up common objects
 
 function Ninja() {
@@ -6,14 +48,11 @@ function Ninja() {
     }
 }
 
-
 var ninja1 = new Ninja();
 var ninja2 = new Ninja();
 
 console.assert(ninja1.stulk() === ninja1,"this 1st ninja is stulking");
 console.assert(ninja2.stulk() === ninja2,"this 2nd ninja is stulking");
-
-
 
 
 
@@ -32,6 +71,5 @@ var ninja1 = {
 var ninja2 = {
     getMyThis : whatsMyContext
 }
-
 
 console.assert(whatsMyContext() === window,"Function call on window");
