4cca0cf975d37d7a305b035742a2912b2ec82865
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-01-16 22:59:34 +0630

    computed property

---
+++ computeProperty.js
@@ -0,0 +1,36 @@
+// let LAST_NAME = "lastname";
+
+// let fullname = {
+//     firstName : "Hein Wai",
+//     [LAST_NAME] : "Yan Htet"
+// }
+
+// console.log(
+//     "My fullname is: " + fullname.firstName
+//     + " " + fullname.lastname
+// )
+
+
+// other way
+
+let ANOTHER_NAME = "lastname";
+let fullname = {
+    firstName : "firstname"
+}
+fullname[ANOTHER_NAME] = "another";
+
+console.log(
+    "My full name is " + fullname.firstName 
+    + " " + fullname[ANOTHER_NAME]
+)
+
+
+// let LAST_NAME = "lastname";
+// let fullname = {
+//     firstname: "somya"
+// };
+// fullname[LAST_NAME] = "jain";
+// console.log(
+//     "My fullname is: " + fullname.firstname
+//     + " " + fullname.lastname
+// );
